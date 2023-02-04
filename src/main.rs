5afec01708d09e54e011fdb7c332c13aba86859e
Mauro Pandolfo <pandolfomauro@gmail.com>
2023-02-04 00:35:10 -0300

    modularization

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,8 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-
-fn die(e: std::io::Error) {
-    println!("Error: {}", e);
-    std::process::exit(1);
-}
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+use editor::Editor;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?} \r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                },
-                Key::Ctrl('r') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => die(err),
-        }
-    }
+    let editor = Editor::default();
+    editor.run();
 }
