438bbdf9ea09a52c34dbb79077a4ea7a1f12ca19
Mauro Pandolfo <pandolfomauro@gmail.com>
2023-02-04 11:10:24 -0300

    welcome message added

--- src/editor.rs
+++ src/editor.rs
@@ -1,15 +1,16 @@
-use std::io::{self, stdout, Write};
+use crate::Terminal;
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor{
     should_quit: bool,
+    terminal: Terminal,
+
 }
 
 impl Editor {
     pub fn run(&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
         loop {
                 if let Err(error) = self.refresh_screen() {
                     die(error);
@@ -23,33 +24,59 @@ impl Editor {
         }
     }
     pub fn default() -> Self {
-        Self{should_quit: false}
+        Self{
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal")
+        }
     }
-    fn refresh_screen(&self) -> Result<(), std::io::Error>{
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1,1));
-        io::stdout().flush()
+    fn refresh_screen(&self) -> Result<(),std::io::Error>{
+        Terminal::cursor_hide();
+        Terminal::cursor_position(0,0);
+        if self.should_quit{
+            Terminal::clear_screen();
+            println!("S O  L O N G  S U C K E R");
+        }else{
+            self.draw_rows();
+            Terminal::cursor_position(0,0);
+        }
+        Terminal::cursor_show();
+        Terminal::flush()
 
     }
 
     fn process_keypress(&mut self) -> Result<(),std::io::Error>{
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('r') => self.should_quit = true,
             _ => (),
         }
         Ok(())
     }
-}
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Susi -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}~", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
+    }
 
-fn read_key() -> Result<Key, std::io::Error>{
-    loop{
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
+    fn draw_rows(&self) {
+        let height = self.terminal.size().height;
+        for row in 0..height {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_message();
+            }else{
+                println!("~\r");        
+            }
         }
     }
 }
 
-
 fn die(e: std::io::Error) {
+    Terminal::clear_screen();
     panic!("{}", e);
 }
